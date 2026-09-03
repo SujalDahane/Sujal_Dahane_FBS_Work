e053a4a69b3a3e6915092b5ac676747e87e62ab1
Sujal Dahane <sujaldahane@gmail.com>
2026-09-03 16:34:59 +0530

    Add files via upload

---
+++ FBS/Core Python/Looping/looping_keywords.py
@@ -0,0 +1,23 @@
+#1.pass : Neglect the expected indentation error
+# for i in range (1,10):
+#     pass
+
+#2. break : for terminating the loop 
+# for i in range (1,10):
+#     if (i==4):
+#         break
+#     print(i)
+
+#3.continue : to stop perticular iteration
+# for i in range (1,10):
+#     if (i==4):
+#         continue
+#     print(i)
+    
+#4.else : will execute when loopexecuted successfully
+# for i in range (1,10):
+#     if (i==4):
+#         break
+#     print(i)
+# else:
+#     print('Else block executed')    
